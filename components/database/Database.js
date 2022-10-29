1dd1a10f77bee63d38cccd9a0fe54cb04bd14457
Your Name <mollenwambui@gmail.com>
2022-10-29 14:44:52 +0300

    first commit

---
+++ components/database/Database.js
@@ -0,0 +1,127 @@
+export const  COLOURS={
+    white:'#FFFFFF',
+    black:'#000000',
+    green:'#00FF00',
+    red:'#FF0000',
+    blue:'#0000FF',
+    backgroundLight:'#F0F0F3',
+    backgroundMedium:'#B9B9B9',
+    backgroundDark:'777777',
+}
+
+export const Items=[
+    {
+        id:1,
+        category:'product',
+        productName:'Jordan Shoes',
+        productPrice:3500,
+        description:'Aesthetic cute shoes ||pink||for all genders',
+        issOff:true,
+        offPercentage:10,
+        productImage:require('../database/images/products/pinks.png'),
+        productImageList:[
+            require('../database/images/products/pinks.png'),
+            require('../database/images/products/pinks.png'),
+            require('../database/images/products/pinks.png'),
+    
+        ]
+
+
+
+    },
+    {
+        id:2,
+        category:'product',
+        productName:'Jordan Shoes',
+        productPrice:4500,
+        description:'Aesthetic cute shoes ||pink||for all genders',
+        issOff:true,
+        offPercentage:10,
+        productImage:require('../database/images/products/pinky.png'),
+        productImageList:[
+            require('../database/images/products/pinks.png'),
+            require('../database/images/products/pinks.png'),
+            require('../database/images/products/pinks.png'),
+       
+        ]
+
+
+
+    },
+    {
+        id:3,
+        category:'shoes',
+        productName:'Jordan Shoes',
+        productPrice:3500,
+        description:'Aesthetic cute shoes ||pink||for all genders',
+        issOff:true,
+        offPercentage:10,
+        productImage:require('../database/images/products/pinkys.png'),
+        productImageList:[
+            require('../database/images/products/pinks.png'),
+            require('../database/images/products/pinks.png'),
+            require('../database/images/products/pinks.png'),
+        
+        ]
+
+
+
+    },
+    {
+        id:4,
+        category:'shoes',
+        productName:'Nike Shoes',
+        productPrice:4500,
+        description:'Aesthetic cute shoes ||pink||for all genders',
+        issOff:true,
+        offPercentage:10,
+        productImage:require('../database/images/shoes/pinks.png'),
+        productImageList:[
+            require('../database/images/shoes/pinkys.png'),
+            require('../database/images/shoes/pinkys.png'),
+            require('../database/images/shoes/pinks.png'),
+         
+        ]
+
+
+
+    },
+    {
+        id:5,
+        category:'shoes',
+        productName:'Nike Shoes',
+        productPrice:3500,
+        description:'Aesthetic cute shoes ||pink||for all genders',
+        issOff:true,
+        offPercentage:10,
+        productImage:require('../database/images/shoes/pinks.png'),
+        productImageList:[
+            require('../database/images/shoes/pinkys.png'),
+            require('../database/images/shoes/pinkys.png'),
+            require('../database/images/shoes/pinkys.png'),,
+           
+        ]
+
+
+
+    },
+    {
+        id:6,
+        category:'shoes',
+        productName:'Nike Shoes',
+        productPrice:4500,
+        description:'Aesthetic cute shoes ||pink||for all genders',
+        issOff:true,
+        offPercentage:10,
+        productImage:require('../database/images/products/pinks.png'),
+        productImageList:[
+            require('../database/images/products/pinks.png'),
+            require('../database/images/products/pinky.png'),
+            require('../database/images/products/pinkys.png'),
+           
+        ]
+
+
+
+    },
+]
